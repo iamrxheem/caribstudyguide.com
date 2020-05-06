7a0b086dfa8f7855a574c6975bc10fc413a30cce
Raheem McDonald <raheem@Raheems-MacBook-Pro.local>
2020-05-06 14:08:22 -0500

    Added in User components

--- src/components/userDropdownMenu.js
+++ src/components/userDropdownMenu.js
@@ -33,9 +33,21 @@ class UserDropdownMenu extends React.Component {
             <i className="fas fa-user text-white"></i>
           </DropdownToggle>
           <DropdownMenu right={true}>
-            <DropdownItem href="/">Account</DropdownItem>
-            <DropdownItem href="/">Another action</DropdownItem>
-            <DropdownItem href="/">Something else here</DropdownItem>
+            <DropdownItem href="/account/me">
+              <i style={{ color: "#305F72" }} className="far fa-user mr-3"></i>
+              My Account
+            </DropdownItem>
+            <DropdownItem href="/account/achievements">
+              <i
+                style={{ color: "#ffb515" }}
+                className="fas fa-trophy mr-3"
+              ></i>
+              Achievements
+            </DropdownItem>
+            <DropdownItem href="/account/resources">
+              <i style={{ color: "#4f3a65" }} class="fas fa-bookmark mr-3"></i>
+              Saved Resources
+            </DropdownItem>
 
             <hr />
             <DropdownItem href="/logout/">Log out</DropdownItem>
