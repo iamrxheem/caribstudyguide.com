4dc5ea80a0ab2d9e7ee704f6ae0a3e39e3937715
Raheem McDonald <raheem@Raheems-MacBook-Pro.local>
2020-05-06 20:09:50 -0500

    Updates

--- src/components/userDropdownMenu.js
+++ src/components/userDropdownMenu.js
@@ -7,11 +7,21 @@ import {
   Button
 } from "shards-react"
 
+// React device detect
+import {
+  BrowserView,
+  MobileView,
+  CustomView,
+  isBrowser,
+  isMobile
+} from "react-device-detect"
+
 class UserDropdownMenu extends React.Component {
   constructor(props) {
     super(props)
+
     this.toggle = this.toggle.bind(this)
-    this.state = { open: false }
+    this.state = { open: false, isLoggedIn: false }
   }
 
   toggle() {
@@ -23,36 +33,64 @@ class UserDropdownMenu extends React.Component {
   render() {
     return (
       <>
-        <Dropdown open={this.state.open} toggle={this.toggle} group>
-          <DropdownToggle
-            style={{
-              backgroundColor: "transparent",
-              borderColor: "transparent"
-            }}
-          >
-            <i className="fas fa-user text-white"></i>
-          </DropdownToggle>
-          <DropdownMenu right={true}>
-            <DropdownItem href="/account/me">
-              <i style={{ color: "#305F72" }} className="far fa-user mr-3"></i>
-              My Account
-            </DropdownItem>
-            <DropdownItem href="/account/achievements">
-              <i
-                style={{ color: "#ffb515" }}
-                className="fas fa-trophy mr-3"
-              ></i>
-              Achievements
-            </DropdownItem>
-            <DropdownItem href="/account/resources">
-              <i style={{ color: "#4f3a65" }} class="fas fa-bookmark mr-3"></i>
-              Saved Resources
-            </DropdownItem>
+        {/* Displays when the user is logged in  */}
+        <CustomView condition={this.state.isLoggedIn}>
+          <Dropdown open={this.state.open} toggle={this.toggle} group>
+            <DropdownToggle
+              style={{
+                backgroundColor: "transparent",
+                borderColor: "transparent"
+              }}
+            >
+              <i className="fas fa-user text-white"></i>
+            </DropdownToggle>
+            <DropdownMenu right={true}>
+              <DropdownItem href="/account/me">
+                <i
+                  style={{ color: "#305F72" }}
+                  className="far fa-user mr-3"
+                ></i>
+                My Account
+              </DropdownItem>
+              <DropdownItem href="/account/achievements">
+                <i
+                  style={{ color: "#ffb515" }}
+                  className="fas fa-trophy mr-3"
+                ></i>
+                Achievements
+              </DropdownItem>
+              <DropdownItem href="/account/resources">
+                <i
+                  style={{ color: "#4f3a65" }}
+                  class="fas fa-bookmark mr-3"
+                ></i>
+                Saved Resources
+              </DropdownItem>
 
-            <hr />
-            <DropdownItem href="/logout/">Log out</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+              <hr />
+              <DropdownItem href="/logout/">Log out</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </CustomView>
+
+        <CustomView condition={!this.state.isLoggedIn}>
+          <Dropdown open={this.state.open} toggle={this.toggle} group>
+            <DropdownToggle
+              style={{
+                backgroundColor: "transparent",
+                borderColor: "transparent"
+              }}
+            >
+              <i className="fas fa-user text-white"></i>
+            </DropdownToggle>
+            <DropdownMenu right={true}>
+              <DropdownItem href="/login/">Log in</DropdownItem>
+
+              <hr />
+              <DropdownItem href="/register/">Create an account</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </CustomView>
       </>
     )
   }
