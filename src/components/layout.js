2d40f820bc1860558391afa46e1beff7dc1f4681
Raheem McDonald <raheem@Raheems-MacBook-Pro.local>
2020-05-08 13:54:59 -0500

    Updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,6 +16,7 @@ import "../assets/css/bulma-tabs.css"
 import "../assets/css/bulma-menu.css"
 import "./layout.css"
 
+import "../assets/js/g-tag-manager.js"
 import "../assets/js/tawkto.js"
 import "../assets/js/matomo.js"
 import "../assets/js/onesignal.js"
@@ -41,6 +42,12 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>{children}</div>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-NWGL3N3"
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      ></iframe>
       <br />
       <Footer />
     </>
